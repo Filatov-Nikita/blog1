7883572ceb157d26ff468cea84bc292d07f27547
Nikita <nikita-filatov51@yandex.ru>
2017-11-03 22:41:42 +0500

    Поменял главную

--- resources/views/pages/main.blade.php
+++ resources/views/pages/main.blade.php
@@ -1,21 +1,15 @@
-   <div class="main_page">
+        <div class="main_page">
             <div class="wrapper">
                 <div class="main_slang">
-                    <p>Блог Филатова Никиты</p>
+                    <p>Блог Филатова Никита</p>
                 </div>
             </div>
         </div>
         <div class="main_content">
             <div class="wrapper">
                 <div class="different_block">
-                    <a  href = "{{ route('site.main.about') }}" class="about_block"><div class="text">Обо мне</div></a>
-                    <a href = "{{ route('site.main.portfolio') }}" class="portfolio_block"><div class="text">Портфолио</div></a>
+                    <a  href = "#" class="about_block"><div class="text">Обо мне</div></a>
+                    <a href = "#" class="portfolio_block"><div class="text">Портфолио</div></a>
                 </div>
             </div>
-        </div>
-        <div class="last_work">
-            <div class="wrapper">
-                <div>Мой последний проект</div>
-            </div>
-        </div>
-        <div class="last_work_block"></div>
+        </div>>-->
