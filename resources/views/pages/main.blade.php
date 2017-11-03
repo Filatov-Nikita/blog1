4755f8d8588f0f19a07e98bf3902314cda8278bb
Nikita <nikita-filatov51@yandex.ru>
2017-11-03 22:49:05 +0500

    Make Links

--- resources/views/pages/main.blade.php
+++ resources/views/pages/main.blade.php
@@ -8,8 +8,8 @@
         <div class="main_content">
             <div class="wrapper">
                 <div class="different_block">
-                    <a  href = "{{ route('site.main.index') }}" class="about_block"><div class="text">Обо мне</div></a>
-                    <a href = "{{ route('site.main.about') }}" class="portfolio_block"><div class="text">Портфолио</div></a>
+                    <a  href = "{{ route('site.main.about') }}" class="about_block"><div class="text">Обо мне</div></a>
+                    <a href = "{{ route('site.main.portfolio') }}" class="portfolio_block"><div class="text">Портфолио</div></a>
                 </div>
             </div>
         </div>>
