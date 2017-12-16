77d08eab280d44844eb7c4063a632915cbdeb347
Nikita <nikita45454@gmail.com>
2017-12-16 16:02:44 +0500

    sdsd

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,17 +1,18 @@
 $(function () {
     $('.menuS').fnMenu();
-    $('.menu .bookmark ul.outMenu li a').on('click', function(e) {
+
+    $('.menu .bookmark ul.outMenu>li>a').on('click', function(e) {
+        e.stopPropagation();
         e.preventDefault();
         var answer = $(this).parent().find('.innerMenu').stop(true).slideToggle();
     });
-	
-		var menu = $('.main .left_panel');
-	$(document).on('scroll', function() {
-		menu.removeClass('fixed');
-		if($(document).scrollTop() > 0) {
-			menu.addClass('fixed');
-			
-		}
-	});
-	
+    var menu = $('.main .menu');
+    $(document).on('scroll', function() {
+        menu.removeClass('fixed');
+        if($(document).scrollTop() > 0) {
+            menu.addClass('fixed');
+
+        }
+    });
+
 });
