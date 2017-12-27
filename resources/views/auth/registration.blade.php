016eb04a560cbc6bbc323d20e9b785510304170d
Filatov-Nikita <nekit_040498@mail.ru>
2017-12-27 14:27:29 +0500

    sadsd

--- resources/views/auth/registration.blade.php
+++ resources/views/auth/registration.blade.php
@@ -5,9 +5,12 @@
             {{ csrf_field() }}
             <label>Имя*<input type="text" name = "name" value = "{{old('name')}}"></label>
             @if ($errors->has('name'))
-            <div>лвлвал</div>
+            <div>Некорректно введенное имя</div>
             @endif
             <label>Email*<input type="text" name = "email" value = "{{old('email')}}"></label>
+            @if ($errors->has('email'))
+                <div>Некорректно введенное email</div>
+            @endif
             <label>Пароль <input type="password" name = "password"></label>
             <label>Повтор пароля <input type="password" name = "password_confirmation"></label>
             <input type="submit" value = "Регистрация" class = "btn">
