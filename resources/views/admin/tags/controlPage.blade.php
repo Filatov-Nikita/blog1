f0e04c7470aa442198e5e1612bceeb4e3c26476a
Nikita Filatov <nikita45454@gmail.com>
2018-06-05 22:45:59 +0500

    fixed

--- resources/views/admin/tags/controlPage.blade.php
+++ resources/views/admin/tags/controlPage.blade.php
@@ -1,9 +1,9 @@
 @extends('admin.base')
   @section('right_column')
   <div class="namePage">Управление тегами</div>
-	<div class="form">
-		
-	</div>
+  <div class="form">
+	  	@include('admin.forms.tags')
+  </div>
     <div class="list">
       <ul>
         @include('admin.tags.listTags')
