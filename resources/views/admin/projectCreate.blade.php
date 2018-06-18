a08b65427a27e964f95983d19585a88ae482293e
nikita <nikita45454@gmail.com>
2018-06-18 10:32:06 +0500

    sd

--- resources/views/admin/projectCreate.blade.php
+++ resources/views/admin/projectCreate.blade.php
@@ -3,6 +3,6 @@
     <div class="header">Добавление нового проекта</div>
     <div class="help_header">Внесите в форму данные для создания нового проекта,<br> все поля необходимы для заполнения !</div>
     <div class="form">
-      @include('admin.form.project')
+      @include('admin.forms.project')
     </div>
 @endsection
