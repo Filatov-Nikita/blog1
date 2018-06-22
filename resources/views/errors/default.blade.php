600bbcf5a6c3eff12ca9d2ff22765e85cce64c71
Nikita Filatov <nikita45454@gmail.com>
2018-06-22 10:11:33 +0500

    fix

--- resources/views/errors/default.blade.php
+++ resources/views/errors/default.blade.php
@@ -1,1 +1,10 @@
-Error {{$errorCode}} {{$errorMessage}}
+<style>
+    body {
+        background: #dc6655;
+        color:#fff;
+        font-size: 48px;
+        font-family: roboto;
+    }
+</style>
+Error {{$errorCode}} {{$errorMessage}} <br>
+Попробуйте начать с <a href="{{url('/')}}">главной</a>!
